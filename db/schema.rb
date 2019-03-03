1ed2ecc214ee88dde892fd9fd9085a83abcc756a
Tayyab Iqbal <tayyab12308@gmail.com>
2019-03-03 23:35:33 +0000

    Add colorpicker component

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_15_223412) do
+ActiveRecord::Schema.define(version: 2019_03_02_115835) do
 
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_01_15_223412) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.boolean "admin", default: false
+    t.text "color"
   end
 
 end
